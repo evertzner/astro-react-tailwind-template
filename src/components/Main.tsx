7b252d0d26c6da18f8a977596adc1d7ea601a5b9
evertzner <esteban.vertzner@gmail.com>
2024-09-09 15:12:42 +0200

    remove space

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,6 @@
 const Main = () => (
   <main>
-    <h1 className='text-2xl text-center'>Welcome to template by Esteban Vertzner</h1>{' '}
+    <h1 className='text-2xl text-center'>Welcome to template by Esteban Vertzner</h1>
   </main>
 );
 
